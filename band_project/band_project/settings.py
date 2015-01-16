8bd5973bbf9dcdb6a78346c0e06626bab2a234fc
Ryan Batchelder <rb688@nau.edu>
2015-01-16 09:58:04 -0700

    Found more places that my mis-named directory was haunting the settings file

--- band_project/band_project/settings.py
+++ band_project/band_project/settings.py
@@ -48,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'CS399.urls'
+ROOT_URLCONF = 'band_project.urls'
 
-WSGI_APPLICATION = 'CS399.wsgi.application'
+WSGI_APPLICATION = 'band_project.wsgi.application'
 
 
 # Database
