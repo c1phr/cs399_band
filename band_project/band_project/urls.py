a2c5c1735045ae1633cad1c561460e0483fd6b03
Ryan Batchelder <rb688@nau.edu>
2015-01-18 12:16:45 -0700

    Added band bio page

--- band_project/band_project/urls.py
+++ band_project/band_project/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 # from django.contrib import admin
+from views import home, meet
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', 'views.home', name='home'),
+     url(r'^$', home, name='home'),
+     url(r'^meet/', meet),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
--- band_project/views.py
+++ band_project/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 # Create your views here.
+
+def meet(request):
+    return render(request, 'meet.html')
