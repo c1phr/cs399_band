b79acf9c1a29c79661be9b14ca023bd88e5ce92d
Duke Ayers <dfa24@nau.edu>
2015-01-17 20:34:25 -0700

    Index Page Created
    Created the initial page, working to get the static folder up and
    running.

--- band_project/band_project/urls.py
+++ band_project/band_project/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 # from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'CS399.views.home', name='home'),
+     url(r'^$', 'views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
